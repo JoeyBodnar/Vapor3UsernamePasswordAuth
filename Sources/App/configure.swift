8779ae0cae47b4731a10965f0594e4003dadd6ab
Stephen Bodnar <anapaix@Apple-Macintosh-2.local>
2018-05-12 10:13:58 +0700

    adding authentication provider to services

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,11 @@
 import FluentPostgreSQL
 import Vapor
-
+import Authentication
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
     try services.register(FluentPostgreSQLProvider())
+    try services.register(AuthenticationProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
