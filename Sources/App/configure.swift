e949027a5e3e7479353c056da39f266dd755b27f
Stephen Bodnar <anapaix@Apple-Macintosh-2.local>
2018-05-12 10:14:39 +0700

    adding auth provier to services

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,7 +5,8 @@ import Vapor
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
     try services.register(FluentPostgreSQLProvider())
-
+    try services.register(AuthenticationProvider())
+    
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
