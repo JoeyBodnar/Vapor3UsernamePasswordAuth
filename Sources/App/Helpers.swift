0eccd60fc09dbcfedf86c3b226507ab63afedca2
Stephen Bodnar <anapaix@Apple-Macintosh-2.local>
2018-05-12 20:16:11 +0700

    removing some characters from list in randomtoken method

--- Sources/App/Helpers.swift
+++ Sources/App/Helpers.swift
@@ -10,7 +10,7 @@ import Foundation
 class Helpers {
     // credit for this randomToken method: https://stackoverflow.com/questions/26845307/generate-random-alphanumeric-string-in-swift
     class func randomToken(withLength length: Int) -> String {
-        let allowedChars = "$!@&#abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let allowedChars = "$!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         let allowedCharsCount = UInt32(allowedChars.count)
         var randomString = ""
         for _ in 0..<length {
