f3857d96d4399330bde6c1147f93b59a4073f186
Stephen Bodnar <anapaix@Apple-Macintosh-2.local>
2018-05-12 08:49:52 +0700

    adding routes for usercontroller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,5 +6,9 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    let userController = UserController()
+    router.get("createUser", use: userController.createUser)
+    router.get("loginUser", use: userController.loginUser)
 
 }
